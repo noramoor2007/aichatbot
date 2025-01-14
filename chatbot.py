de4f538f317ec3c74ffc87f894a6d1a22e50abe0
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2025-01-13 21:14:51 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,3 +3,6 @@ responses = { # Here, we are defining a dictionary of questions and responses
     "how are you": "I'm just a bot, but I'm functioning as expected!",
     "bye": "Goodbye! Have a great day!",
 }
+def chatbot(input_text): # We are using a function called chatbot to handle the input from the user
+    input_text = input_text.lower()
+    return responses.get(input_text, "Sorry, I don't understand that.")
