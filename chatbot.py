b1163ba4359ba398262918b416f7d73368e03f99
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2025-01-13 21:14:43 -0500

    Update and rename chatbot.ipynb to chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,5 @@
+responses = { # Here, we are defining a dictionary of questions and responses
+    "hello": "Hi there! How can I help you?",
+    "how are you": "I'm just a bot, but I'm functioning as expected!",
+    "bye": "Goodbye! Have a great day!",
+}
