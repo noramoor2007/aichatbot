cfc08d3b87b645073b5b625ca476841c90d57706
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2025-01-13 21:05:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import nbformat
 
+with open('path/to/notebook.ipynb', 'r') as f:
+    nb = nbformat.read(f, as_version=4)
+
+with open('path/to/fixed_notebook.ipynb', 'w') as f:
+    nbformat.write(nb, f)
